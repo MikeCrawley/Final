b1097f7918817ca98f9a5614eac818846879f3fb
MikeCrawley <mmc6@cdc.gov>
2016-05-05 22:29:38 -0400

    update

--- final.sql
+++ final.sql
@@ -41,7 +41,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES ('nhtrfe3','Goblin pops','great',5,8,75,NULL,'great','looks good'),('23','hot',NULL,23,23,23,NULL,'dogs','hamp.bmp'),('232','knife',NULL,12,14,12,NULL,'sharp','cool.jpg'),('234','scarf',NULL,1,100,2,NULL,'red','redscarf.jpg'),('1234','red',NULL,3,4,3,NULL,'crazycat','34.p'),('45','gun',NULL,300,500,2,NULL,'45cal',NULL);
+INSERT INTO `products` VALUES ('nhtrfe3','Goblin pops','great',5,8,76,NULL,'great','/Final/slideshow/scarf1.jpg'),('23','hot',NULL,23,23,24,NULL,'dogs','/Final/slideshow/scarf2.jpg'),('232','knife',NULL,12,14,12,NULL,'sharp','/Final/slideshow/scarf3.jpg'),('234','scarf',NULL,1,100,2,NULL,'red','/Final/slideshow/scarf4.jpg'),('1234','red',NULL,3,4,3,NULL,'crazycat','/Final/slideshow/scarf6.jpg'),('45','gun',NULL,300,500,2,NULL,'45cal','/Final/slideshow/scarf1.jpg');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +59,7 @@ CREATE TABLE `user` (
   `datecreated` datetime DEFAULT NULL,
   `username` varchar(90) DEFAULT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COMMENT='			';
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='			';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +68,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'m','YsZqel3XDDFGYYBjw0TlMebUtZ43mAhEPOlis6vWPFo=','2016-04-29 00:00:00','m'),(2,'z','WU5RmuSZMSspQzt92Kl/8Gje/LqXVbbV0A6ExSTWewY=','2016-04-29 00:00:00','z'),(3,'z','WU5RmuSZMSspQzt92Kl/8Gje/LqXVbbV0A6ExSTWewY=','2016-04-29 00:00:00','z'),(4,'q','jjXCzTv2ZBvbDiBQt2kyy7LmA0oN2swdm+qCprpX988=','2016-04-29 00:00:00','q'),(5,'x','LXEWQrcmsEQBYnyp+6wy9chTD7GQPMTbAiWHF5IaSIE=','2016-04-29 00:00:00','x'),(6,'hairy@com','XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=','2016-04-29 00:00:00','justhairy'),(7,'z','WU5RmuSZMSspQzt92Kl/8Gje/LqXVbbV0A6ExSTWewY=','2016-04-29 00:00:00','z'),(8,'a','ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=','2016-04-29 00:00:00','a'),(9,'mmc6@cdc.gov','XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=','2016-04-29 00:00:00','Mike'),(10,'k','glTDKakoUPbVOd03b0gW7idkUX2l4CNVFK9DMWRIDXo=','2016-04-29 00:00:00','k'),(11,'p','fLzNpllZpP5inb31Rq493qkyiuWlNJh4X0onOU/iZRU=','2016-04-29 00:00:00','p');
+INSERT INTO `user` VALUES (1,'m','YsZqel3XDDFGYYBjw0TlMebUtZ43mAhEPOlis6vWPFo=','2016-04-29 00:00:00','m'),(2,'z','WU5RmuSZMSspQzt92Kl/8Gje/LqXVbbV0A6ExSTWewY=','2016-04-29 00:00:00','z'),(3,'z','WU5RmuSZMSspQzt92Kl/8Gje/LqXVbbV0A6ExSTWewY=','2016-04-29 00:00:00','z'),(4,'q','jjXCzTv2ZBvbDiBQt2kyy7LmA0oN2swdm+qCprpX988=','2016-04-29 00:00:00','q'),(5,'x','LXEWQrcmsEQBYnyp+6wy9chTD7GQPMTbAiWHF5IaSIE=','2016-04-29 00:00:00','x'),(6,'hairy@com','XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=','2016-04-29 00:00:00','justhairy'),(7,'z','WU5RmuSZMSspQzt92Kl/8Gje/LqXVbbV0A6ExSTWewY=','2016-04-29 00:00:00','z'),(8,'a','ypeBEsobvcr6wjGzmiPcTaeG7/gUfE5yuYB3ha/uSLs=','2016-04-29 00:00:00','a'),(9,'mmc6@cdc.gov','XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=','2016-04-29 00:00:00','Mike'),(10,'k','glTDKakoUPbVOd03b0gW7idkUX2l4CNVFK9DMWRIDXo=','2016-04-29 00:00:00','k'),(11,'p','fLzNpllZpP5inb31Rq493qkyiuWlNJh4X0onOU/iZRU=','2016-04-29 00:00:00','p'),(12,'x','LXEWQrcmsEQBYnyp+6wy9chTD7GQPMTbAiWHF5IaSIE=','2016-05-03 00:00:00','x'),(13,'i','3n0bchoeBjK3zwTt9QMsjs/6n5oISSFSuSbxpafnZdc=','2016-05-05 00:00:00','i');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -81,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-01 12:56:34
+-- Dump completed on 2016-05-05 22:29:02
